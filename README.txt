6c67ce4724533c1c67124ed6e2f40a7374c9f89a
Quentin Sharier <sharier.2@osu.edu>
2023-02-27 10:59:48 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+GPT Capstone Project
+
+This program currently only has the capability to act as a GPT chat bot. It uses an openai API key to send queries.
+
+1. You must install openai bindings before running. "pip install openai"
+2. Run main.py
+3. Entrer "exit" to terminate program.
---
+++ main.py
@@ -0,0 +1,40 @@
+import openai
+
+APIKEY = "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"
+
+# Establish connection with openai
+openai.api_key = APIKEY
+
+# Function that sends openai a query and returns gpt-3's response
+def get_gpt_response(query):
+
+    # Get response from GPT-3
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=query,
+        # Temperature 0-1, 0 Least risk, 1 most risk
+        temperature=0.6,
+        max_tokens=500
+    )
+    return response.choices[0].text
+
+
+def main():
+
+    # Continue to ask user for input until 'exit' is entered
+   while True:
+       print("Enter your question for GPT.")
+       query  = input("Your Query: ")
+       if query.lower() == "exit":
+           break
+       else :
+            response = get_gpt_response(query)
+            print("\nGPT:")
+            print(response + "\n")
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
